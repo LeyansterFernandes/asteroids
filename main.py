2f675ba7152074a337d61d9bd10606e98125b271
lee <leyansterfwf@gmail.com>
2024-12-16 21:46:58 +0000

    Added display

--- main.py
+++ main.py
@@ -1,19 +1,31 @@
 import pygame
 import sys
 
+# Initialise All Pygame Modules
 pygame.init()
 
-#Main game loop
+# Main game loop
 screen = pygame.display.set_mode((400,500))
 clock = pygame.time.Clock()
 framerate = 60
-
+test_surface = pygame.Surface((100,200))
+test_surface.fill((0,0,255))
+x = 100
+y = 200
+w = 100
+h = 100
+test_rect = test_surface.get_rect(center = (200,250))
+x_pos = 200
+y_pos = 200
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit
 
+    screen.fill((175,215,70))
+    pygame.draw.rect(screen,pygame.Color('red'),test_rect)
+    screen.blit(test_surface,test_rect)
     pygame.display.update()
     clock.tick(framerate)
 
