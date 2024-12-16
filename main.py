de74ee4e00530b4688e894043898169153361e58
lee <leyansterfwf@gmail.com>
2024-12-16 22:21:10 +0000

    Added FRUIT on the grid and initial layout

--- main.py
+++ main.py
@@ -1,11 +1,38 @@
 import pygame
+from pygame.math import Vector2
 import sys
+import random
 
+
+
+
+class FRUIT:
+    def __init__(self):
+        # Randomly determine an x and y position
+        self.x = random.randint(0,cell_number - 1) # Subtracting 1 to show that we are always on screen ( within boundaries )
+        self.y = random.randint(0,cell_number - 1 )
+        
+        # Store x and y coordinates on a vector
+        self.pos = Vector2(self.x,self.y)
+        # draw a square
+        
+    def draw_fruit(self):
+        # Create Rectangle ( Scale x and y to match grid size)
+        x_pos = self.pos.x * cell_size
+        y_pos = self.pos.y * cell_size
+        fruit_rect = pygame.Rect(int (x_pos), int (y_pos), cell_size, cell_size)
+        pygame.draw.rect(screen, (126,166,114), fruit_rect)
+        
+        # draw the rectangle
+        
+       
 # Initialise All Pygame Modules
 pygame.init()
+cell_size = 40
+cell_number = 20
 
 # Main game loop
-screen = pygame.display.set_mode((400,500))
+screen = pygame.display.set_mode((cell_number * cell_size, cell_size * cell_number))
 clock = pygame.time.Clock()
 framerate = 60
 test_surface = pygame.Surface((100,200))
@@ -14,9 +41,9 @@ x = 100
 y = 200
 w = 100
 h = 100
-test_rect = test_surface.get_rect(center = (200,250))
-x_pos = 200
-y_pos = 200
+
+fruit = FRUIT()
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -24,8 +51,7 @@ while True:
             sys.exit
 
     screen.fill((175,215,70))
-    pygame.draw.rect(screen,pygame.Color('red'),test_rect)
-    screen.blit(test_surface,test_rect)
+    fruit.draw_fruit()
     pygame.display.update()
     clock.tick(framerate)
 
